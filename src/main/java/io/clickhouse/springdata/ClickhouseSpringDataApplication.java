ba119bc09d94508698abf1a8d205544ea4a71631
s.ladygin <s.a.ladygin@yandex.ru>
2025-03-15 22:16:20 +0300

    Init commit

---
+++ src/main/java/io/clickhouse/springdata/ClickhouseSpringDataApplication.java
@@ -0,0 +1,13 @@
+package io.clickhouse.springdata;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ClickhouseSpringDataApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ClickhouseSpringDataApplication.class, args);
+    }
+
+}
---
+++ src/test/java/io/clickhouse/springdata/ClickhouseSpringDataApplicationTests.java
@@ -0,0 +1,13 @@
+package io.clickhouse.springdata;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ClickhouseSpringDataApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
