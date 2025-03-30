0c462f026c47eeeba56c569fc5b44f9993dc0711
s.ladygin <s.a.ladygin@yandex.ru>
2025-03-30 15:58:07 +0300

    disable bootjar

--- build.gradle.kts
+++ build.gradle.kts
@@ -70,3 +70,11 @@ publishing {
         }
     }
 }
+
+tasks.getByName<Jar>("jar") {
+    enabled = true
+}
+
+tasks.getByName<org.springframework.boot.gradle.tasks.bundling.BootJar>("bootJar") {
+    enabled = false
+}
